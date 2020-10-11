68b58a94113b8b133750cef82ce596385636e544
Syasya <syasyazman2000@gmail.com>
2020-10-11 13:57:20 +0800

    Edit tests for delete command and parser to test both client and order deletion

--- src/test/java/seedu/address/logic/parser/DeleteClientCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteClientCommandParserTest.java
@@ -11,17 +11,17 @@ import seedu.address.logic.commands.DeleteClientCommand;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the DeleteCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the DeleteCommand, and therefore we test only one of them.
+ * outside of the DeleteClientCommand code. For example, inputs "1" and "1 abc" take the
+ * same path through the DeleteClientCommand, and therefore we test only one of them.
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */
-public class DeleteCommandParserTest {
+public class DeleteClientCommandParserTest {
 
     private DeleteClientCommandParser parser = new DeleteClientCommandParser();
 
     @Test
-    public void parse_validArgs_returnsDeleteCommand() {
+    public void parse_validArgs_returnsDeleteClientCommand() {
         assertParseSuccess(parser, "1", new DeleteClientCommand(INDEX_FIRST_CLIENT));
     }
 
@@ -30,3 +30,4 @@ public class DeleteCommandParserTest {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteClientCommand.MESSAGE_USAGE));
     }
 }
+
