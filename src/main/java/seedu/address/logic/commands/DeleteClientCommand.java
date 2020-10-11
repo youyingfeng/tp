567c21ede0857eb10384364540f4c2261ed81c80
Syasya <syasyazman2000@gmail.com>
2020-10-11 15:07:27 +0800

    Edit import statements

--- src/main/java/seedu/address/logic/commands/DeleteClientCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteClientCommand.java
@@ -5,12 +5,11 @@ import static seedu.address.logic.parser.CliSyntax.DELETE_PREFIX_CLIENT;
 
 import java.util.List;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Client;
-
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.exceptions.CommandException;
 
 /**
  * Deletes a person identified using it's displayed index from the address book.
--- src/main/java/seedu/address/logic/commands/DeleteOrderCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteOrderCommand.java
@@ -5,12 +5,11 @@ import static seedu.address.logic.parser.CliSyntax.DELETE_PREFIX_ORDER;
 
 import java.util.List;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Order;
-
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.exceptions.CommandException;
 
 /**
  * Deletes a person identified using it's displayed index from the address book.
