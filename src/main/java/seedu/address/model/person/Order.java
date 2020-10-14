d984dd30d2ea66bc2495e092d76d99e4a696beac
Syasya <syasyazman2000@gmail.com>
2020-10-14 15:18:23 +0800

    Add order list and relevant classes

--- src/main/java/seedu/address/model/person/Order.java
+++ src/main/java/seedu/address/model/person/Order.java
@@ -6,9 +6,24 @@ public class Order {
     // feel free to change - tx
     private boolean isDone = false;
 
+    // function to be removed
     public Name getName() {
         // not implemented yet
         return null;
+    }
+
+    /**
+     * Returns true if both orders of the same id have at least one other identity field that is the same.
+     * This defines a weaker notion of equality between two orders.
+     */
+    public boolean isSameOrder(Order otherOrder) {
+        if (otherOrder == this) {
+            return true;
+        }
+
+        // to be edited
+        return otherOrder != null
+                && otherOrder.getName().equals(getName());
     }
 
     /**
---
+++ src/main/java/seedu/address/model/person/UniqueOrderList.java
@@ -0,0 +1,136 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.person.exceptions.DuplicateOrderException;
+import seedu.address.model.person.exceptions.OrderNotFoundException;
+
+/**
+ * A list of persons that enforces uniqueness between its elements and does not allow nulls.
+ * A person is considered unique by comparing using {@code Person#isSamePerson(Person)}. As such, adding and updating of
+ * persons uses Person#isSamePerson(Person) for equality so as to ensure that the person being added or updated is
+ * unique in terms of identity in the UniquePersonList. However, the removal of a person uses Person#equals(Object) so
+ * as to ensure that the person with exactly the same fields will be removed.
+ *
+ * Supports a minimal set of list operations.
+ *
+ * @see Order#isSameOrder(Order)
+ */
+public class UniqueOrderList implements Iterable<Order> {
+    private final ObservableList<Order> internalList = FXCollections.observableArrayList();
+    private final ObservableList<Order> internalUnmodifiableList =
+            FXCollections.unmodifiableObservableList(internalList);
+
+    /**
+     * Returns true if the list contains an equivalent person as the given argument.
+     */
+    public boolean contains(Order toCheck) {
+        requireNonNull(toCheck);
+        return internalList.stream().anyMatch(toCheck::isSameOrder);
+    }
+
+    /**
+     * Adds a person to the list.
+     * The person must not already exist in the list.
+     */
+    public void add(Order toAdd) {
+        requireNonNull(toAdd);
+        if (contains(toAdd)) {
+            throw new DuplicateOrderException();
+        }
+        internalList.add(toAdd);
+    }
+
+    /**
+     * Replaces the person {@code target} in the list with {@code editedPerson}.
+     * {@code target} must exist in the list.
+     * The person identity of {@code editedPerson} must not be the same as another existing person in the list.
+     */
+    public void setOrder(Order target, Order editedOrder) {
+        requireAllNonNull(target, editedOrder);
+
+        int index = internalList.indexOf(target);
+        if (index == -1) {
+            throw new OrderNotFoundException();
+        }
+
+        if (!target.isSameOrder(editedOrder) && contains(editedOrder)) {
+            throw new DuplicateOrderException();
+        }
+
+        internalList.set(index, editedOrder);
+    }
+
+    /**
+     * Removes the equivalent person from the list.
+     * The person must exist in the list.
+     */
+    public void remove(Order toRemove) {
+        requireNonNull(toRemove);
+        if (!internalList.remove(toRemove)) {
+            throw new OrderNotFoundException();
+        }
+    }
+
+    public void setOrders(UniqueOrderList replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
+    /**
+     * Replaces the contents of this list with {@code persons}.
+     * {@code persons} must not contain duplicate persons.
+     */
+    public void setOrders(List<Order> orders) {
+        requireAllNonNull(orders);
+        if (!ordersAreUnique(orders)) {
+            throw new DuplicateOrderException();
+        }
+
+        internalList.setAll(orders);
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<Order> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
+    }
+
+    @Override
+    public Iterator<Order> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof UniqueOrderList // instanceof handles nulls
+                && internalList.equals(((UniqueOrderList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
+    /**
+     * Returns true if {@code persons} contains only unique persons.
+     */
+    private boolean ordersAreUnique(List<Order> orders) {
+        for (int i = 0; i < orders.size() - 1; i++) {
+            for (int j = i + 1; j < orders.size(); j++) {
+                if (orders.get(i).isSameOrder(orders.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
