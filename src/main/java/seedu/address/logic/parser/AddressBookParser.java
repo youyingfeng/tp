7daf7fe3c01cbc22e15bc50e38809664a83c77e1
cx0810 <chengxin0810@gmail.com>
2020-10-15 12:03:17 +0800

    Update relevant classes for list orders

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,18 +6,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.ClientCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteClientCommand;
-import seedu.address.logic.commands.DeleteOrderCommand;
-import seedu.address.logic.commands.DoneCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -70,6 +59,9 @@ public class AddressBookParser {
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
+
+        case ListOrderCommand.COMMAND_WORD:
+            return new ListOrderCommand();
 
         case ExitCommand.COMMAND_WORD:
             return new ExitCommand();
