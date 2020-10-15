64c0aa0899a25ab801f6e863ce53db793295f893
You Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2020-10-15 18:31:44 +0800

    Refactor Order to use UniqueOrderList

--- src/main/java/seedu/address/logic/commands/OrderCommand.java
+++ src/main/java/seedu/address/logic/commands/OrderCommand.java
@@ -1,18 +1,12 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.commons.core.Messages.MESSAGE_CREATE_ORDER_SUCCESS;
+import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.ORDER_PREFIX_CLIENT;
 import static seedu.address.logic.parser.CliSyntax.ORDER_PREFIX_DESCRIPTION;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
-import java.util.List;
-
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Client;
 import seedu.address.model.person.Order;
 
 /**
@@ -28,58 +22,37 @@ public class OrderCommand extends Command {
             + "CLIENT_ID (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " " + ORDER_PREFIX_DESCRIPTION + " shoes " + ORDER_PREFIX_CLIENT + " 123";
 
-    public static final String MESSAGE_ARGUMENTS = "Description: %1$s, Client id: %2$d";
+    public static final String MESSAGE_SUCCESS = "New client added: %1$s";
+    public static final String MESSAGE_DUPLICATE_ORDER = "This client already exists in LogOnce";
 
-    private final Index clientId;
-    private final String description;
+    private final Order toAdd;
 
     /**
-     * @param description of the order
-     * @param clientId of the person the order is linked to
+     * Creates an OrderCommand to add the specified {@code Order}
      */
-    public OrderCommand(String description, Index clientId) {
-        requireAllNonNull(description, clientId);
+    public OrderCommand(Order order) {
+        requireAllNonNull(order);
 
-        this.description = description;
-        this.clientId = clientId;
+        this.toAdd = order;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        List<Client> lastShownList = model.getFilteredPersonList();
+        requireNonNull(model);
 
-        if (clientId.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX);
+        if (model.hasOrder(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_ORDER);
         }
 
-        Client personToEdit = lastShownList.get(clientId.getZeroBased());
-        List<Order> orderList = personToEdit.getOrderList();
-        orderList.add(new Order(description, clientId));
-        Client editedPerson = new Client(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
-                personToEdit.getAddress(), model, personToEdit.getTags(), orderList);
-
-        model.setPerson(personToEdit, editedPerson);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-
-        return new CommandResult(MESSAGE_CREATE_ORDER_SUCCESS);
+        model.addOrder(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
     }
 
 
     @Override
     public boolean equals(Object other) {
-        // short circuit if same object
-        if (other == this) {
-            return true;
-        }
-
-        // instanceof handles nulls
-        if (!(other instanceof OrderCommand)) {
-            return false;
-        }
-
-        // state check
-        OrderCommand e = (OrderCommand) other;
-        return clientId.equals(e.clientId)
-                && description.equals(e.description);
+        return other == this // short circuit if same object
+                || (other instanceof OrderCommand // instanceof handles nulls
+                && toAdd.equals(((OrderCommand) other).toAdd)); // same as ClientCommand's equals
     }
 }
