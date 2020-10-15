eb0f6261e14272fd2ef7d512818d7e727614d80f
cx0810 <chengxin0810@gmail.com>
2020-10-15 15:35:07 +0800

    Update Model.java

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -11,113 +11,111 @@ import seedu.address.model.person.Order;
 /**
  * The API of the Model component.
  */
-public abstract class Model {
+public interface Model {
     /** {@code Predicate} that always evaluate to true */
-    public static final Predicate<Client> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
-    public static final Predicate<Order> PREDICATE_SHOW_ALL_ORDERS = unused -> true;
+    Predicate<Client> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
 
     /**
      * Replaces user prefs data with the data in {@code userPrefs}.
      */
-    public abstract void setUserPrefs(ReadOnlyUserPrefs userPrefs);
+    void setUserPrefs(ReadOnlyUserPrefs userPrefs);
 
     /**
      * Returns the user prefs.
      */
-    public abstract ReadOnlyUserPrefs getUserPrefs();
+    ReadOnlyUserPrefs getUserPrefs();
 
     /**
      * Returns the user prefs' GUI settings.
      */
-    public abstract GuiSettings getGuiSettings();
+    GuiSettings getGuiSettings();
 
     /**
      * Sets the user prefs' GUI settings.
      */
-    public abstract void setGuiSettings(GuiSettings guiSettings);
+    void setGuiSettings(GuiSettings guiSettings);
 
     /**
      * Returns the user prefs' address book file path.
      */
-    public abstract Path getAddressBookFilePath();
+    Path getAddressBookFilePath();
 
     /**
      * Sets the user prefs' address book file path.
      */
-    public abstract void setAddressBookFilePath(Path addressBookFilePath);
+    void setAddressBookFilePath(Path addressBookFilePath);
 
     /**
      * Replaces address book data with the data in {@code addressBook}.
      */
-    public abstract void setAddressBook(ReadOnlyAddressBook addressBook);
+    void setAddressBook(ReadOnlyAddressBook addressBook);
 
     /** Returns the AddressBook */
-    public abstract ReadOnlyAddressBook getAddressBook();
+    ReadOnlyAddressBook getAddressBook();
 
     /**
      * Returns true if a person with the same identity as {@code person} exists in the address book.
      */
-    public abstract boolean hasPerson(Client client);
+    boolean hasPerson(Client client);
 
     /**
      * Deletes the given person.
      * The person must exist in the address book.
      */
-    public abstract void deletePerson(Client target);
+    void deletePerson(Client target);
 
     /**
      * Adds the given person.
      * {@code person} must not already exist in the address book.
      */
-    public abstract void addPerson(Client client);
+    void addPerson(Client client);
 
     /**
      * Replaces the given person {@code target} with {@code editedPerson}.
      * {@code target} must exist in the address book.
      * The person identity of {@code editedPerson} must not be the same as another existing person in the address book.
      */
-    public abstract void setPerson(Client target, Client editedClient);
+    void setPerson(Client target, Client editedClient);
 
     /** Returns an unmodifiable view of the filtered person list */
-    public abstract ObservableList<Client> getFilteredPersonList();
+    ObservableList<Client> getFilteredPersonList();
 
     /**
      * Updates the filter of the filtered person list to filter by the given {@code predicate}.
      * @throws NullPointerException if {@code predicate} is null.
      */
-    public abstract void updateFilteredPersonList(Predicate<Client> predicate);
-
+    void updateFilteredPersonList(Predicate<Client> predicate);
 
     /**
      * Returns true if a order with the same identity as {@code order} exists in the order book.
      */
-    public abstract boolean hasOrder(Order client);
+    boolean hasOrder(Order client);
 
     /**
      * Deletes the given order.
      * The order must exist in the order book
      */
-    public abstract void deleteOrder(Order target);
+    void deleteOrder(Order target);
 
     /**
      * Adds the given order.
      * {@code order} must not already exist in the order book.
      */
-    public abstract void addOrder(Order order);
+    void addOrder(Order order);
 
     /**
      * Replaces the given order {@code target} with {@code editedOrder}.
      * {@code target} must exist in the order book.
      * The person identity of {@code editedOrder} must not be the same as another existing order in the order book.
      */
-    public abstract void setOrder(Order target, Order editedOrder);
+    void setOrder(Order target, Order editedOrder);
 
     /** Returns an unmodifiable view of the filtered order list */
-    public abstract ObservableList<Order> getFilteredOrderList();
+    ObservableList<Order> getFilteredOrderList();
 
     /**
      * Updates the filter of the filtered order list to filter by the given {@code predicate}.
      * @throws NullPointerException if {@code predicate} is null.
      */
-    public abstract void updateFilteredOrderList(Predicate<Order> predicate);
+    void updateFilteredOrderList(Predicate<Order> predicate);
 }
