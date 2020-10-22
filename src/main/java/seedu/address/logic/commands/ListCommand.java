7de25654a43096a3e3e97434d1f5fcccca7622dc
cx0810 <chengxin0810@gmail.com>
2020-10-22 16:51:17 +0800

    Add functionality for list order and list client commands to switch panels

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -15,11 +15,11 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
-
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        MainWindow.getInstance().handleClients();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
--- src/main/java/seedu/address/logic/commands/ListOrderCommand.java
+++ src/main/java/seedu/address/logic/commands/ListOrderCommand.java
@@ -15,11 +15,11 @@ public class ListOrderCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all orders";
 
-
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredOrderList(PREDICATE_SHOW_ALL_ORDERS);
+        MainWindow.getInstance().handleOrders();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
