b4fe2b9280801200cb65f65d1a0729bd4754bfe4
cx0810 <chengxin0810@gmail.com>
2020-10-22 17:44:23 +0800

    Add logging to ListCommand

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -3,6 +3,9 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import seedu.address.model.Model;
 import seedu.address.ui.MainWindow;
 
@@ -15,11 +18,15 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
+    private static Logger logger = Logger.getLogger("list");
+
     @Override
     public CommandResult execute(Model model) {
+        logger.log(Level.INFO, "start processing");
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         MainWindow.getInstance().handleClients();
+        logger.log(Level.INFO, "end processing");
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
