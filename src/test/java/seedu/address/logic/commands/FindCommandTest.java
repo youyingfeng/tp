5a107a943ec433777603d384ef7e70677d3f116a
Ting Xiao <tigerting98@gmail.com>
2020-10-29 21:50:01 +0800

    Fix JUnit test style violations

--- src/test/java/seedu/address/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindCommandTest.java
@@ -5,9 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.CARL;
-import static seedu.address.testutil.TypicalPersons.ELLE;
-import static seedu.address.testutil.TypicalPersons.FIONA;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.ArrayList;
@@ -31,34 +28,41 @@ public class FindCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
-//    @Test
-//    public void equals() {
-//        NameContainsKeywordsPredicate firstPredicate =
-//                new NameContainsKeywordsPredicate(Collections.singletonList("first"));
-//        NameContainsKeywordsPredicate secondPredicate =
-//                new NameContainsKeywordsPredicate(Collections.singletonList("second"));
-//
-//        ArrayList<>
-//
-//        FindCommand findFirstCommand = new FindCommand(firstPredicate);
-//        FindCommand findSecondCommand = new FindCommand(secondPredicate);
-//
-//        // same object -> returns true
-//        assertTrue(findFirstCommand.equals(findFirstCommand));
-//
-//        // same values -> returns true
-//        FindCommand findFirstCommandCopy = new FindCommand(firstPredicate);
-//        assertTrue(findFirstCommand.equals(findFirstCommandCopy));
-//
-//        // different types -> returns false
-//        assertFalse(findFirstCommand.equals(1));
-//
-//        // null -> returns false
-//        assertFalse(findFirstCommand.equals(null));
-//
-//        // different person -> returns false
-//        assertFalse(findFirstCommand.equals(findSecondCommand));
-//    }
+    @Test
+    public void equals() {
+        NameContainsKeywordsPredicate firstPredicate =
+                new NameContainsKeywordsPredicate(Collections.singletonList("first"));
+        NameContainsKeywordsPredicate secondPredicate =
+                new NameContainsKeywordsPredicate(Collections.singletonList("second"));
+
+        ArrayList<Predicate<Client>>firstList = new ArrayList<>();
+        ArrayList<Predicate<Client>> secondList = new ArrayList<>();
+
+        firstList.add(firstPredicate);
+        secondList.add((secondPredicate));
+
+        ClientMultiPredicate firstMulti = new ClientMultiPredicate(firstList);
+        ClientMultiPredicate secondMulti = new ClientMultiPredicate(secondList);
+
+        FindCommand findFirstCommand = new FindCommand(firstMulti);
+        FindCommand findSecondCommand = new FindCommand(secondMulti);
+
+        // same object -> returns true
+        assertTrue(findFirstCommand.equals(findFirstCommand));
+
+        // same values -> returns true
+        FindCommand findFirstCommandCopy = new FindCommand(firstMulti);
+        assertTrue(findFirstCommand.equals(findFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(findFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(findFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(findFirstCommand.equals(findSecondCommand));
+    }
 
     @Test
     public void execute_zeroKeywords_noPersonFound() {
