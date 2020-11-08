7402b2da4a79d5e020faf5380c4ce497d8e66b40
cx0810 <chengxin0810@gmail.com>
2020-11-08 17:06:54 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/model/VersionedAddressBookTest.java
+++ src/test/java/seedu/address/model/VersionedAddressBookTest.java
@@ -1,6 +1,9 @@
 package seedu.address.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.AMY;
 import static seedu.address.testutil.TypicalPersons.BOB;
