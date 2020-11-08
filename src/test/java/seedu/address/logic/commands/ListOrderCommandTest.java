8fe9e0d310e07980434b127ef19f0980605df6bd
cx0810 <chengxin0810@gmail.com>
2020-11-08 10:14:21 +0800

    Add ListCommandTest and ListOrderCommandTest

--- src/test/java/seedu/address/logic/commands/ListOrderCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListOrderCommandTest.java
@@ -5,22 +5,31 @@ import static seedu.address.logic.commands.CommandTestUtil.showClientAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.io.TempDir;
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.LogicManager;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.storage.*;
+import seedu.address.ui.MainWindow;
+
+import java.nio.file.Path;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
  */
-public class ListCommandTest {
+public class ListOrderCommandTest {
+
+    @TempDir
+    public Path testFolder;
 
     private Model model;
     private Model expectedModel;
-
     private CommandHistory commandHistory = new CommandHistory();
 
     @BeforeEach
@@ -31,12 +40,12 @@ public class ListCommandTest {
 
     @Test
     public void execute_listIsNotFiltered_showsSameList() {
-        assertCommandSuccess(new ListCommand(), model, commandHistory, ListCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ListOrderCommand(), model, commandHistory, ListOrderCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
     public void execute_listIsFiltered_showsEverything() {
         showClientAtIndex(model, INDEX_FIRST_CLIENT);
-        assertCommandSuccess(new ListCommand(), model, commandHistory, ListCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ListOrderCommand(), model, commandHistory, ListOrderCommand.MESSAGE_SUCCESS, expectedModel);
     }
 }
