77b72be04550ff355c1360ca6bd70e8b826e186e
xnmng <puaxuanmingpxm18@gmail.com>
2020-11-09 06:40:56 +0800

    add tests for UpdateOrderCommand

--- src/main/java/seedu/address/logic/parser/UpdateClientCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UpdateClientCommandParser.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.UPDATE_CLIENT_PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.UPDATE_CLIENT_PREFIX_CLIENTID;
 import static seedu.address.logic.parser.CliSyntax.UPDATE_CLIENT_PREFIX_EMAIL;
