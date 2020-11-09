5a9c745b606397ac2a7b474da2bc688bb946d138
You Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2020-11-09 17:30:17 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindCommandTest.java
@@ -1,32 +1,23 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Optional;
 import java.util.function.Predicate;
 
-import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.LogicManager;
-import seedu.address.model.*;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Client;
 import seedu.address.model.person.ClientMultiPredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
-import seedu.address.storage.Storage;
-import seedu.address.ui.MainWindow;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code FindCommand}.
--- src/test/java/seedu/address/logic/commands/FindOrderCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindOrderCommandTest.java
@@ -1,10 +1,7 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.commons.core.Messages.MESSAGE_ORDERS_LISTED_OVERVIEW;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.ArrayList;
